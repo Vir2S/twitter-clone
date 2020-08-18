c2b813483e784ddd0ffabd9f35ae1093decfa109
Vitaly Sem <virtualik@gmail.com>
2020-08-19 01:16:37 +0300

    registration view added

--- src/users/views.py
+++ src/users/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
 
-# Create your views here.
+from .forms import UserRegisterForm, UserUpdateForm, ProfileUpdateForm
+
+
+def register(request):
+
+    if request.method == 'POST':
+        form = UserRegisterForm(request.POST)
+
+        if form.is_valid():
+            form.save()
+
+            username = form.cleaned_data.get('username')
+            messages.success(request, f'Account for {username} successfully created.')
+
+            return redirect('login')
+
+    else:
+        form = UserRegisterForm()
+
+    return render(request, 'users/register.html', {'form': form})
