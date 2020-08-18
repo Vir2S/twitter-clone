3eb233525329bc098988550c57f1948544e4e6b4
Vitaly Sem <virtualik@gmail.com>
2020-08-19 01:38:53 +0300

    profile view added

--- src/users/views.py
+++ src/users/views.py
@@ -15,7 +15,11 @@ def register(request):
             form.save()
 
             username = form.cleaned_data.get('username')
-            messages.success(request, f'Account for {username} successfully created.')
+
+            messages.success(
+                request,
+                f'Account for {username} successfully created.'
+            )
 
             return redirect('login')
 
@@ -23,3 +27,38 @@ def register(request):
         form = UserRegisterForm()
 
     return render(request, 'users/register.html', {'form': form})
+
+
+@login_required
+def profile(request):
+
+    if request.method == 'POST':
+
+        user_form = UserUpdateForm(
+            request.POST,
+            instance=request.user
+        )
+
+        profile_form = ProfileUpdateForm(
+            request.POST,
+            request.FILES,
+            instance=request.user.profile
+        )
+
+        if user_form.is_valid() and profile_form.is_valid():
+
+            user_form.save()
+            profile_form.save()
+
+            messages.success(
+                request,
+                f'Account successfully updated'
+            )
+
+            return redirect('profile')
+
+    else:
+        user_form = UserUpdateForm(instance=request.user)
+        profile_form = ProfileUpdateForm(instance=request.user.profile)
+
+    return render(request, 'users/profile.html', {'user_form': user_form, 'profile_form': profile_form})
