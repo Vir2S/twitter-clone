51585d44cdef99896bd9fc7ebfdb8cebdf8a7c1c
Vitaly Sem <virtualik@gmail.com>
2020-08-20 00:58:41 +0300

    create_profile notification added

--- src/users/notifications.py
+++ src/users/notifications.py
@@ -6,3 +6,8 @@ from django.dispatch import receiver
 
 from .models import Profile
 
+# notification that gets fired after the user is saved
+@receiver(save_post, sender=User)
+def create_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
