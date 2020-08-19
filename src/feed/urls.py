85048d751b59aaa26618f889df1b88f2c0cb44cb
Vitaly Sem <virtualik@gmail.com>
2020-08-20 01:47:58 +0300

    feed urls fixed

--- src/feed/urls.py
+++ src/feed/urls.py
@@ -11,7 +11,7 @@ from .views import (
     postpreference)
 
 urlpatterns = [
-    path('', PostListView.as_view(), name='blog-home'),
+    path('', PostListView.as_view(), name='feed-home'),
     path('post/new/', PostCreateView.as_view(), name='post-create'),
     path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
     path('user/<str:username>', UserPostListView.as_view(), name='user-posts'),
