e202421cd0be7c33c3f9989b935689e40902b728
Vitaly Sem <virtualik@gmail.com>
2020-08-20 01:16:44 +0300

    Follow model added

--- src/users/models.py
+++ src/users/models.py
@@ -32,3 +32,10 @@ class Profile(models.Model):
             output_size = (300, 300)
             img.thumbnail(output_size)
             img.save(self.image.path)
+
+
+class Follow(models.Model):
+
+    user = models.ForeignKey(User, related_name='user', on_delete=models.CASCADE)
+    follow_user = models.ForeignKey(User, related_name='follow_user', on_delete=models.CASCADE)
+    date = models.DateTimeField(auto_now_add=True)
