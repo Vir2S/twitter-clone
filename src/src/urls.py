7207f9f268fa612ed740c5cc5a639466f0466f1d
Vitaly Sem <virtualik@gmail.com>
2020-08-20 00:41:22 +0300

    urls modified

--- src/src/urls.py
+++ src/src/urls.py
@@ -14,8 +14,44 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.contrib.auth import views as auth_views
+
+from django.urls import path, include
+
+from django.conf import settings
+from django.conf.urls.static import static
+
+from users import views as user_views
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+
+    path('login/', auth_views.LoginView.as_view(
+        template_name='users/login.html'),
+         name='login'),
+
+    path('logout/', auth_views.LogoutView.as_view(
+        template_name='users/logout.html'),
+         name='logout'),
+
+    path('password-reset/', auth_views.PasswordResetView.as_view(
+        template_name='users/password_reset.html'),
+         name='password-reset'),
+
+    path('password-reset/done', auth_views.PasswordResetDoneView(
+        template_name='users/password_reset_done.html'),
+         name='password-reset-done'),
+
+    path('password-reset/confirm/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(
+        template_name='users/password_reset_confirm.html'),
+         name='password-reset-done'),
+
+    path('register/', user_views.register, name='user-register'),
+    path('profile/', user_views.profile, name='profile'),
+    path('search/', user_views.search_view, name='search'),
+    path('', include('feed.urls'))
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
