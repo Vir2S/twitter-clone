68537aca89bd6bd1c55f4a7114ace611bfa3824f
Vitaly Sem <virtualik@gmail.com>
2020-08-27 21:45:26 +0300

    users admin added

--- src/users/admin.py
+++ src/users/admin.py
@@ -1,3 +1,10 @@
-from django.contrib import admin
+from django.apps import AppConfig
 
-# Register your models here.
+
+class UsersConfig(AppConfig):
+
+    name = 'users'
+
+    def ready(self):
+
+        import users.signals
